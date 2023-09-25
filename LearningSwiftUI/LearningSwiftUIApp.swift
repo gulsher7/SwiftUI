21a4b1e284bb9ad7a26c6db5d00405f254b8f97b
Gulsher7 <gulsherkhan0005@gmail.com>
2023-09-25 13:28:22 +0530

    Initial Commit

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Gulsher Khan on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
